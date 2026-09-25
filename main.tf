c2ca0da1e8370c6326afcc2a7921e7794a7d9f38
karthik sai Beri <57845597+beri95@users.noreply.github.com>
2026-09-25 16:27:39 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,11 +3,11 @@ provider "aws" {
 }
 
 resource "aws_instance" "web_server" {
+  count         =   2
   ami           = "ami-066c4849e6b3a1e3d"
   instance_type = "t2.micro"
           
   tags = {
     Name = "My-server"
   }
-
 }
